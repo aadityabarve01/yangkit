736d89de16e9f385894d363fb705782cfefcb642
aadityabarve01 <aadityabarve01@gmail.com>
2024-08-22 01:51:54 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,10 +8,7 @@ INSTALL_REQUIREMENTS = [
     'Jinja2==3.0.3'
 ]
 
-if sys.version.lower().startswith('3.11'):
-    INSTALL_REQUIREMENTS.append('lxml==4.9.3')
-else:
-     INSTALL_REQUIREMENTS.append('lxml==3.4.4') 
+INSTALL_REQUIREMENTS.append('lxml==4.9.3') 
 
 setup(
     name='yangkit',
